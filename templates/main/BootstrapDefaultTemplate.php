8e177e32fb39b274c341483c81a62d14f95770dd
Rafał B <bieronski@yahoo.pl>
2021-06-04 12:02:24 +0200

    version 0.9.2 realease

--- templates/main/BootstrapDefaultTemplate.php
+++ templates/main/BootstrapDefaultTemplate.php
@@ -38,10 +38,10 @@
             <?php for($iRow = 0; $iRow <= $this->data['rowsCount']; $iRow++): ?>
             <tr>
                 <?php if ($this->data['shouldRenderRow'][$iRow]): ?>
-                    <td colspan="<?php echo $this->data['columnsCount']; ?>" class="alert alert-danger">
+                    <td colspan="<?php print $this->data['columnsCount']; ?>" class="alert alert-danger">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#8b0000" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
                             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                        </svg>Błąd wiersza - w tym wierszu znajdują się błędne dane
+                        </svg> Błąd wiersza - w tym wierszu znajdują się błędne dane
                     </td>
                 <?php else: ?>
                     <?php foreach ($this->data['cells'][$iRow] as $key => $value): ?>
@@ -62,42 +62,40 @@
 
     <!-- PAGINATION PART -->
     <?php if ($this->data['pagination']['shouldRender']): ?>
-    <div class="text-center">
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
-                <!-- PREVIOUS BUTTON -->
-                <?php if ($this->data['pagination']['previousButton']['isActive']): ?>
-                <li class="page-item">
-                <?php else: ?>
-                <li class="page-item disabled">
-                <?php endif; ?>
-                    <a class="page-link" href="<?php echo $this->data['pagination']['previousButton']['url']; ?>"><u>Previous</u></a>
-                </li>
-
-                <!-- PAGES -->
-                <?php for($iPage = 1; $iPage <= $this->data['pagesCount']; $iPage++): ?>
+        <div class="text-center">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <!-- PREVIOUS BUTTON -->
+                    <?php if ($this->data['pagination']['previousButton']['isActive']): ?>
+                    <li class="page-item">
+                    <?php else: ?>
+                    <li class="page-item disabled">
+                        <?php endif; ?>
+                        <a class="page-link" href="<?php echo $this->data['pagination']['previousButton']['url']; ?>"><u>Previous</u></a>
+                    </li>
+                    <!-- PAGES -->
+                    <?php for($iPage = 1; $iPage <= $this->data['pagesCount']; $iPage++): ?>
                     <?php if ($this->data['pagination'][$iPage]['isActive']): ?>
                     <li class="page-item">
                     <?php else: ?>
                     <li class="page-item active">
                     <?php endif; ?>
-                        <a class="page-link" href="<?php echo $this->data['pagination'][$iPage]['url']; ?>">
-                            <u><?php echo $this->data['pagination'][$iPage]['text']; ?></u>
-                        </a>
+                    <a class="page-link" href="<?php echo $this->data['pagination'][$iPage]['url']; ?>">
+                        <u><?php echo $this->data['pagination'][$iPage]['text']; ?></u>
+                    </a>
                     </li>
-                <?php endfor; ?>
-
-                <!-- NEXT BUTTON -->
-                <?php if ($this->data['pagination']['nextButton']['isActive']): ?>
-                <li class="page-item">
+                    <?php endfor; ?>
+                    <!-- NEXT BUTTON -->
+                    <?php if ($this->data['pagination']['nextButton']['isActive']): ?>
+                    <li class="page-item">
                     <?php else: ?>
-                <li class="page-item disabled">
+                    <li class="page-item disabled">
                     <?php endif; ?>
-                    <a class="page-link" href="<?php echo $this->data['pagination']['nextButton']['url']; ?>"><u>Next</u></a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+                        <a class="page-link" href="<?php echo $this->data['pagination']['nextButton']['url']; ?>"><u>Next</u></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     <?php endif; ?>
 </div>
 <?php endif; ?>
